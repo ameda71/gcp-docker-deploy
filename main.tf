a8829bc73c18f4768bd0174194082ea21432c17a
ameda71 <amedasaiteja75@gmail.com>
2025-02-12 12:50:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,53 +1,36 @@
 provider "google" {
-  # Path to GCP service account key
-  project     = var.project_id                             # GCP Project ID
-  region      = var.region                                  # GCP region, e.g., "us-central1"
+  credentials = file("cherr.json")
+  project     = "<YOUR-PROJECT-ID>"
+  region      = "us-central1"
 }
 
-resource "google_compute_instance" "docker_instance" {
-  name         = "docker-vm-instance"
-  machine_type = "e2-medium"
+resource "google_compute_instance" "web_server" {
+  name         = "web-server"
+  machine_type = "f1-micro"
   zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11-bullseye-v20210916"  # Debian image for VM
+      image = "debian-cloud/debian-10-buster-v20210916"
     }
   }
 
   network_interface {
     network = "default"
     access_config {
-      // This provides the VM with an external IP address
     }
   }
 
   metadata_startup_script = <<-EOT
     #!/bin/bash
     apt-get update
-    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    sh get-docker.sh
-    docker --version
-    docker pull ${var.docker_image}
-    docker run -d -p 80:80 ${var.docker_image}  # Running the Docker container
+    apt-get install -y apache2
+    systemctl start apache2
+    systemctl enable apache2
+    echo "<h1>Welcome to the Web Server</h1>" > /var/www/html/index.html
   EOT
 }
 
-variable "project_id" {
-  description = "The GCP Project ID"
-  type        = string
-  default = "siatejaameda"
-}
-
-variable "region" {
-  description = "The GCP Region"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "docker_image" {
-  description = "Docker Image to Deploy"
-  type        = string
-  default = "sampled"
+output "instance_ip" {
+  value = google_compute_instance.web_server.network_interface[0].access_config[0].nat_ip
 }
